a2a5cdd7abf5375a485701edd2cf071f95763051
Gaëtan Renaudeau <gre@zenexity.com>
2013-11-12 14:42:54 +0100

    fix plugin for latest jekyll

--- _plugins/excerpt.rb
+++ _plugins/excerpt.rb
@@ -1,8 +1,9 @@
 module Jekyll
   class Post
     alias_method :original_to_liquid, :to_liquid
-    def to_liquid
-      original_to_liquid.deep_merge({
+    def to_liquid(attrs = nil)
+      data = (attrs.nil? ? original_to_liquid() : original_to_liquid(attrs))
+      data.deep_merge({
         'excerpt' => content.match('<!-- more -->') ? content.split('<!-- more -->').first : nil
       })
     end
